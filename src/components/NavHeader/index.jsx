61735f0878d37c43424f5819cd38919103354bed
Mallone Matheus <t1wb4n@gmail.com>
2025-03-11 01:36:15 -0300

    Fix

--- src/components/NavHeader/index.jsx
+++ src/components/NavHeader/index.jsx
@@ -1,57 +1,32 @@
-import { Link, useNavigate } from "react-router-dom"
-import imgLogo from "../../assets/logodentista.svg"
-<<<<<<< HEAD
-//import styles from "./index.module.scss"
-=======
-import styles from "./index.module.scss"
-import  navLinks  from "../Mockdata/data"
->>>>>>> 8e0c8e6f0c76e3b11d53048e242f1e2eb9b92369
-export const NavHeader = () => {
-    const navigate = useNavigate()
-    const schedulingButton = () => {
-        navigate("/scheduling")
+import { Link, useNavigate } from "react-router-dom";
+import imgLogo from "../../assets/logodentista.svg";
+import styles from "./index.module.scss";
+import navLinks from "../Mockdata/data";
 
-    }
+export const NavHeader = () => {
+    const navigate = useNavigate();
+
+    const schedulingButton = () => {
+        navigate("/scheduling");
+    };
+
     return (
         <nav>
-<<<<<<< HEAD
-            <ul className="">
-
-                <li>
-                    <Link to="/">Início</Link>
-                </li>
-                <li>
-                    <Link to="/about">Sobre</Link>
-                </li>
-                <li>
-                    <Link to="reviews">Avaliações</Link>
-                </li>
-                <li>
-                    <Link to="/treatments">Tratamentos</Link>
-                </li>
-                <li>
-                    <Link to="/location">Localização</Link>
-                </li>
-
-=======
             <ul className={styles.listMenu_navBox}>
-            <div>
-                <img src={imgLogo} />
-            </div>
+                <div>
+                    <img src={imgLogo} alt="Logo" />
+                </div>
 
-            {navLinks.map((link) => (
-                <li key={link.id}>
-                    <Link to={link.path}>{link.label}</Link>
-                </li>
-            ))}
->>>>>>> 8e0c8e6f0c76e3b11d53048e242f1e2eb9b92369
+                {navLinks.map((link) => (
+                    <li key={link.id}>
+                        <Link to={link.path}>{link.label}</Link>
+                    </li>
+                ))}
+
                 <div>
                     <button onClick={schedulingButton}>Marcar Consulta</button>
                 </div>
-
-
             </ul>
-
         </nav>
-    )
-}
+    );
+};
