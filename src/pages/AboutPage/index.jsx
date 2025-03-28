4cc2fc98e5b81a5b43b7736b59487cd3a9bc6098
Enio Carlos Silva <66915807+enioCarloss@users.noreply.github.com>
2025-03-27 21:37:31 -0300

    add img aboutPage

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -1,9 +1,40 @@
+import { ImageSlider } from "../../components/SliderData/imageSlider"
+import { SliderData } from "../../components/SliderData/sliderData"
+import styles from "./index.module.scss"
+import img from "../../assets/dentista.svg"
 export const AboutPage = () => {
 
-    return(
-        <section>
-            <h1>About Us</h1>
-            <p>L luctus sed vel justo. Sed vel ipsum ac velit tincidunt varius velit ac nulla.</p>
+    return (
+        <section className={styles.about_sectionContainer}>
+            <div className={styles.about_slider}>
+
+                <ImageSlider slides={SliderData} />
+
+            </div>
+            <div className={styles.about_infoContainer}>
+                <h2>Dr. Fulano te Tal</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac justo non mauris luctus tincidunt.
+                    Nulla facilisi. Nulla facilisi. Donec lobortis, felis in dignissim molestie, velit nunc sagittis justo,
+                    eu commodo lectus mauris id massa. Donec vel ante id ipsum consectetur ultricies. Sed non enim vel felis
+                    consectetur, ac dignissim eros laoreet. Duis nec dolor non libero convallis gravida.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac justo non mauris luctus tincidunt.
+                    Nulla facilisi. Nulla facilisi. Donec lobortis, felis in dignissim molestie, velit nunc sagittis justo,
+                    eu commodo lectus mauris id massa. Donec vel ante id ipsum consectetur ultricies. Sed non enim vel felis
+                    consectetur, ac dignissim eros laoreet. Duis nec dolor non libero convallis gravida.
+                </p>
+
+                <div className={styles.about_teamContainer}>
+                    <p>Minha Equipe</p>
+                    <div>
+                        <img src={img} />
+                        <img src={img} />
+                        <img src={img} />
+                    </div>
+
+                </div>
+            </div>
+
         </section>
     )
 }
